83d96fac3cfa481d2b163d1c623309786ea9c070
Ian Castaño <ianfedev@gmail.com>
2026-04-30 22:55:12 -0500

    :q

--- module/analytics/runtime/module.go
+++ module/analytics/runtime/module.go
@@ -13,9 +13,7 @@ import (
 	analyticshttp "mannaiah/module/analytics/adapter/http"
 	analyticsstore "mannaiah/module/analytics/adapter/store"
 	"mannaiah/module/analytics/application"
-	affinityapp "mannaiah/module/analytics/application/affinity"
 	recommendationapp "mannaiah/module/analytics/application/recommendation"
-	rfmapp "mannaiah/module/analytics/application/rfm"
 	"mannaiah/module/analytics/port"
 	corecron "mannaiah/module/core/cron"
 	corehttp "mannaiah/module/core/http"
@@ -41,16 +39,8 @@ type Module struct {
 	cfg Config
 	// service defines analytics use-case dependencies.
 	service *application.AnalyticsService
-	// rfmService defines RFM use-case dependencies.
-	rfmService *rfmapp.RFMService
-	// affinityService defines affinity use-case dependencies.
-	affinityService *affinityapp.AffinityService
 	// handler defines HTTP route adapter dependencies.
 	handler *analyticshttp.Handler
-	// rfmHandler defines RFM HTTP route adapter dependencies.
-	rfmHandler *analyticshttp.RFMHandler
-	// affinityHandler defines affinity HTTP route adapter dependencies.
-	affinityHandler *analyticshttp.AffinityHandler
 	// recommendationService defines recommendation use-case dependencies.
 	recommendationService *recommendationapp.RecommendationService
 	// recommendationHandler defines recommendation HTTP route adapter dependencies.
@@ -109,12 +99,6 @@ func New(cfg Config, db *gorm.DB, registrar bus.Registrar) (*Module, error) {
 
 	module := &Module{cfg: cfg, service: service, handler: handler, clickhouseClient: clickhouseClient}
 
-	if err := module.wireRFM(db); err != nil {
-		return nil, err
-	}
-	if err := module.wireAffinity(store); err != nil {
-		return nil, err
-	}
 	if err := module.wireRecommendation(db); err != nil {
 		return nil, err
 	}
@@ -124,55 +108,6 @@ func New(cfg Config, db *gorm.DB, registrar bus.Registrar) (*Module, error) {
 	}
 
 	return module, nil
-}
-
-// wireRFM wires RFM use-cases and HTTP handler.
-func (m *Module) wireRFM(db *gorm.DB) error {
-	groupRepo, err := analyticsstore.NewRFMGroupRepository(db)
-	if err != nil {
-		return fmt.Errorf("create rfm group repository: %w", err)
-	}
-
-	if m.cfg.Enabled && m.cfg.MigrationEnabled {
-		if seedErr := groupRepo.SeedDefaultBands(context.Background()); seedErr != nil {
-			return fmt.Errorf("seed default rfm bands: %w", seedErr)
-		}
-	}
-
-	var rfmStore port.RFMStore
-	if m.clickhouseClient != nil {
-		rfmStore = clickhouse.NewStoreAdapter(m.clickhouseClient)
-	} else {
-		rfmStore = &noopRFMStore{}
-	}
-
-	rfmSvc, err := rfmapp.NewService(rfmStore, groupRepo)
-	if err != nil {
-		return fmt.Errorf("create rfm service: %w", err)
-	}
-	m.rfmService = rfmSvc
-	m.rfmHandler = analyticshttp.NewRFMHandler(rfmSvc)
-
-	return nil
-}
-
-// wireAffinity wires affinity use-cases and HTTP handler.
-func (m *Module) wireAffinity(store port.Store) error {
-	var affinityStore port.AffinityStore
-	if m.clickhouseClient != nil {
-		affinityStore = clickhouse.NewStoreAdapter(m.clickhouseClient)
-	} else {
-		affinityStore = &noopAffinityStore{}
-	}
-
-	affinitySvc, err := affinityapp.NewService(affinityStore)
-	if err != nil {
-		return fmt.Errorf("create affinity service: %w", err)
-	}
-	m.affinityService = affinitySvc
-	m.affinityHandler = analyticshttp.NewAffinityHandler(affinitySvc)
-
-	return nil
 }
 
 // wireRecommendation wires recommendation use-cases and HTTP handler.
@@ -226,12 +161,6 @@ func (m *Module) SetAuthorizer(authorizer analyticshttp.Authorizer) {
 	if m.handler != nil {
 		m.handler.SetAuthorizer(authorizer)
 	}
-	if m.rfmHandler != nil {
-		m.rfmHandler.SetAuthorizer(authorizer)
-	}
-	if m.affinityHandler != nil {
-		m.affinityHandler.SetAuthorizer(authorizer)
-	}
 	if m.recommendationHandler != nil {
 		m.recommendationHandler.SetAuthorizer(authorizer)
 	}
@@ -244,9 +173,6 @@ func (m *Module) SetSyncRecorder(recorder port.SyncRecorder) {
 	}
 	if m.service != nil {
 		m.service.SetSyncRecorder(recorder)
-	}
-	if m.affinityService != nil {
-		m.affinityService.SetSyncRecorder(recorder)
 	}
 }
 
